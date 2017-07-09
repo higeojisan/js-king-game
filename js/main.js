5e76d171c47efc8566e0d39bbb5a5b2785879338
m-kouda <makotokoda82@gmail.com>
2017-07-09 20:05:08 +0900

    命令の実装

---
+++ js/main.js
@@ -0,0 +1,35 @@
+(function() {
+  'use strict';
+
+  var orders = ["をほめる","をdisる","を殴る","にkissする","にハグする"];
+
+  var numElement = document.getElementById('num');
+  var resultElement = document.getElementById('result');
+  var orderButton = document.getElementById('orderButton');
+
+  orderButton.addEventListener('click', function() {
+    // 人数(num)の取得
+    var num = parseInt(numElement.value);
+    if (isNaN(num)) {
+      resultElement.innerText = '数値を半角で入力してください';
+      resultElement.style.color = 'red';
+      return;
+    }
+    if (num <= 1) {
+      resultElement.innerText = '2以上を入力してください';
+      resultElement.style.color = 'red';
+      return;
+    }
+    var members = [];
+    for (var i = 1; i <= num; i++) {
+      members.push(i);
+    }
+    // 命令の表示
+    var random = Math.floor(Math.random() * orders.length);
+    var order = orders[random];
+    members.sort(function() {return Math.random() - .5});
+    var subject = members[0];
+    var object = members[1];
+    resultElement.innerText = "#" + subject + "が#" + object + order;
+  })
+})();
